121b16c23f008cbb2a53ef3d4316b062b818bfe7
Shilan <dsl273128768@gmail.com>
2025-01-26 19:44:08 -0500

    Write a query that returns all customer purchases of product IDs 4 and 9.

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -18,10 +18,14 @@ LIMIT 10;
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
-
+SELECT *
+FROM customer_purchases
+WHERE product_id IN (4,9);
 
 -- option 2
-
+SELECT *
+FROM customer_purchases
+WHERE product_id = 4 OR product_id = 9;
 
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
